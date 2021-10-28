258520c2f1c42ada99dba03f6ca07994dd7529b6
luisdiego <luispacheco08@hotmail.com>
2021-10-27 19:53:34 -0700

    tiktok server commit

---
+++ modules/DBconfig.js
@@ -0,0 +1,22 @@
+const  data  = [
+    {
+        url:'https://v16m.tiktokcdn.com/2f0f563e1e55ee63a389e91e1fa59963/6178b46c/video/tos/useast2a/tos-useast2a-pve-0068/df169005142e4d5c9b24d67b25d78f40/?a=1233&br=1758&bt=879&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=98CxQex74kag3&l=20211026200656010189195015503AE439&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=am80cjw6ZndxODMzNzczM0ApNzo7aGc7NzxoN2k6ZTM8Z2c1azBxcjRvb2hgLS1kMTZzczYxNWBgMGItX181LjFjNF86Yw%3D%3D&vl=&vr=',
+        chanel: "ziglermonster",
+        song: "I am a Song",
+        likes: "23455",
+        messages: "231",
+        description: 'this is an exapmle description',
+        shares: "20",
+    },
+    {
+        url:'https://v16m.tiktokcdn.com/2f0f563e1e55ee63a389e91e1fa59963/6178b46c/video/tos/useast2a/tos-useast2a-pve-0068/df169005142e4d5c9b24d67b25d78f40/?a=1233&br=1758&bt=879&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=98CxQex74kag3&l=20211026200656010189195015503AE439&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=am80cjw6ZndxODMzNzczM0ApNzo7aGc7NzxoN2k6ZTM8Z2c1azBxcjRvb2hgLS1kMTZzczYxNWBgMGItX181LjFjNF86Yw%3D%3D&vl=&vr=',
+        chanel: "ziglermonster",
+        song: "I am a Song",
+        likes: "23455",
+        messages: "231",
+        description: 'this is an exapmle description',
+        shares: "20",
+    }
+]
+
+module.exports = data;
---
+++ server.js
@@ -0,0 +1,77 @@
+'use strict'
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const data = require('./modules/DBconfig');
+var Videos = require('./modules/dbmodule');
+
+var app = express();
+const port = 9000;
+
+
+
+
+//midelwares
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+
+//CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+//DB config
+const conection_url = 'mongodb+srv://admin:unzkzs0OYBQN4BQt@cluster0.xfzr7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(conection_url).then(() => {
+    console.log(Videos);
+});
+
+//API endpoints
+app.get('/',(req, res) =>
+res.status(200).send('hello world'));
+
+app.get(('/v1/posts'), (req, res) => 
+res.status(200).send(data));
+
+app.get('/v2/posts', (req, res) => {
+    Videos.find({}).exec((err, dat) =>{
+        if(err) return res.status(500).send(err);
+
+        return res.status(200).send(dat);
+    })
+})
+
+app.post('/v2/posts', (req, res) => {
+    var project = Videos();
+    const dbVideos = req.body;
+
+    project.url = dbVideos.url;
+    project.chanel = dbVideos.chanel;
+    project.song = dbVideos.song;
+    project.likes = dbVideos.likes;
+    project.messages = dbVideos.messages;
+    project.description = dbVideos.description;
+    project.shares = dbVideos.shares;
+
+    project.save(dbVideos, (err, dat) => {
+        if (err) return res.status(500).send(err);
+
+        if(!dat) return res.status(404).send('error');
+
+        return res.status(200).send(dat);
+    })
+
+});
+
+//Listening
+app.listen(port, () => 
+console.log(`listening on local host ${port}`))
+
